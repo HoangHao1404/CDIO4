e639bd2a3e81026463265714ba8a8b1c71910e4c
quocvo114 <quocvovan123@gmail.com>
2025-09-21 09:51:37 +0700

    UI Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,21 +10,23 @@ import Sidebar from "./components/common/Sidebar";
 import NavbarAirZen from "./components/common/Navbar";
 import { ThresholdePage } from "./pages/ThresholdePage";
 import { UserManagerment } from "./pages/UserManagerment";
+import { Register } from "./pages/Register";
 
 function App() {
   return (
-    <div className="flex h-screen bg-gray-100">
-      <Sidebar />
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <div className="w-full flex justify-center px-4">
-          <NavbarAirZen />
-        </div>
-        <main className="flex-1 overflow-x-hidden overflow-y-auto">
-          {/* <ThresholdePage /> */}
-          <UserManagerment/>
-        </main>
-      </div>
-    </div>
+    // <div className="flex h-screen bg-gray-100">
+    //   <Sidebar />
+    //   <div className="flex-1 flex flex-col overflow-hidden">
+    //     <div className="w-full flex justify-center px-4">
+    //       <NavbarAirZen />
+    //     </div>
+    //     <main className="flex-1 overflow-x-hidden overflow-y-auto">
+    //       {/* <ThresholdePage /> */}
+    //       {/* <UserManagerment/> */}
+          <Register/>
+    //     </main>
+    //   </div>
+    // </div>
   );
 }
 
