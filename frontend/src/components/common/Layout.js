caf7bafe1a71052c40cfa46c0fe801515acd5b45
quocvo114 <quocvovan123@gmail.com>
2025-10-05 09:28:08 +0700

    update

--- frontend/src/components/common/Layout.js
+++ frontend/src/components/common/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
 
 const SIDEBAR_W = 230; // px -> đúng theo Figma
 const NAVBAR_H = 70; // px -> chiều cao navbar theo Figma
@@ -36,38 +36,10 @@ export default function Layout({ children }) {
         <div className="max-w-[1385px] mx-auto px-8">
           {/* Content Wrapper */}
           <div className="bg-white rounded-3xl shadow-lg  min-h-[calc(107vh-200px)]">
-            {children}
+            <Outlet/>
           </div>
         </div>
       </main>
-=======
-import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
-import NavbarAirZen from "./Navbar";
-
-const SIDEBAR_W = 230;
-const NAVBAR_H = 70;
-
-export default function Layout() {
-  return (
-    <div className="flex min-h-screen bg-[#f3f4f6]">
-      {/* Sidebar */}
-      <Sidebar />
-
-      {/* Phần chính */}
-      <div className="flex-1 flex flex-col ml-[230px]">
-        {/* Navbar (fixed, nằm trên content) */}
-        <NavbarAirZen />
-
-        {/* Nội dung */}
-        <main
-          className="flex-1 p-6 overflow-x-hidden"
-          style={{ marginTop: `${NAVBAR_H + 28}px` }}
-        >
-          <Outlet />
-        </main>
-      </div>
->>>>>>> origin/Tun
     </div>
   );
 }
